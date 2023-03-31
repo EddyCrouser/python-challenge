efdd301a8dc93622f2b02a611bbb33158372d7c4
Eddy-Crouser <ecrou2@pdx.edu>
2023-03-30 18:21:03 -0700

    Created the required programs

--- pybank/main.py
+++ pybank/main.py
@@ -1,1 +1,69 @@
+import os
+import csv
 
+#gives the running total
+tally = 0
+#stores the largest of the previous and current rows to find the max
+big = 0
+#stores the smallest of the previous and current rows to find the min
+small = 0
+#tallies how many rows we go through to find the number of months recorded
+months = 0
+#saves all the values in the second column so I have the first and last month to find the mean change per month
+values = []
+#will find the average change per month
+avg = 0
+#csvpath = os.path.join("./","Resources", "budget_data.csv")
+
+difference = 0
+#opens the budget dataset for use later on
+with open('./Resources/budget_data.csv','r', encoding= 'utf',) as budgetdata:
+
+    intel = csv.reader(budgetdata)
+    print(intel)
+    #skips the first row so that the data can be summed.
+    headerline = next(intel)
+    #runs through the rows, stores all the Profit/Losses data to an array for later, then as it works throught the rows, it makes a running total of profits and losses, and counts how many rows it's run through
+    for row in intel:
+        values.append(row[1])
+        storage = row[1]
+        months = months + 1
+        tally = tally + int(storage)
+            #runs two if statements which compare the previous profit/loss values to the one in the current row then saves the smaller and larger so we have the min and max
+        if int(storage) < small:
+            small = int(storage)
+            smallest = row
+        if int(storage) > big:
+            big = int(storage)
+            biggest = row
+    
+print(values)
+#calculates the mean change per month using (last$-first$)/(#ofmonths-1)
+avg = round((int(values[-1]) - int(values[0]))/(months-1),2)
+#prints the data in the format requested
+print("Data Analytics")
+print()
+print("---------------------------------------")
+print()
+print("Number of Months: " + str(months))
+print()
+print("Total: " + str(tally))
+print()
+print("The average change is: " + str(avg))
+print()
+print("Greatest Increase in Profits: " + biggest[1] + " on " + biggest[0])
+print()
+print("Greatest Decrease in Profits: " + smallest[1] + " on " + smallest[0])
+print()
+
+#writes the information into a text document
+with open('./Analysis/Analytics.txt','wt') as results:
+
+    results.write('Data Analytics\n\n')
+    results.write("---------------------------------------\n\n")
+    results.write("Number of Months: " + str(months) + '\n\n')
+    results.write("Total: " + str(tally) + '\n\n')
+    results.write("The average change is: " + str(avg) + '\n\n')
+    results.write("Greatest Increase in Profits: " + biggest[1] + " on " + biggest[0] + '\n\n')
+    results.write("Greatest Decrease in Profits: " + smallest[1] + " on " + smallest[0])
+    
--- pypoll/main.py
+++ pypoll/main.py
@@ -1,1 +1,80 @@
+import os
+import csv
 
+#used for checking which candidate won
+counter = 0
+#used to calculate the percentage votes for each candidate to be stored in the percentage array
+percentage_var = 0
+#stores all the unique candidates
+candidate = []
+#stores the vote totals for all the candidates
+votes = []
+#stores the percentage of the votes for each candidate
+percentage = []
+#gives the value of the winner based on their array slot
+winner = 0
+
+#opens the voter data for use
+with open('./Resources/election_data.csv','r', encoding= 'utf') as mydata:
+    #reads the data
+    
+    intel = csv.reader(mydata)
+    #skips the first row since the headers aren't needed
+    headerline = next(intel)
+    #runs through the data and adds any candidate not in the array as well as increases the vote counter whenever a candidate comes up in the vote column
+    for row in intel:
+        if row[2] not in candidate:
+            candidate.append(row[2])
+            votes.append('1')
+        else:
+            for i in range(len(candidate)):
+                if row[2] == candidate[i]:
+                    votes[i] = int(votes[i]) + 1
+
+total = 0
+#sums the votes of all candidates to find the total votes cast
+for i in range(len(candidate)):
+    total = total + int(votes[i])
+#calculates the percentage for each candidate then stores them in the percentage array so they correctly correspond to the candidate
+for i in range(len(candidate)):
+    percentage_var = round(100*int(votes[i])/total,3)
+    percentage.append(str(percentage_var))
+
+#starts to print out the results in the requested format
+print()
+print("Election Results:")
+print()
+print('---------------------------------')
+print()
+print('Total Votes: ' + str(total))
+print()
+print('---------------------------------')
+print()
+for i in range(len(candidate)):
+    print(str(candidate[i]) + ": " + str(percentage[i]) + '% (' + str(votes[i]) + ')')
+    print()
+print('---------------------------------')
+print()
+#systematically checks which candidate has the largest number of votes and stores the winner
+counter = int(votes[i])
+for i in range(len(candidate)):
+    if votes[i] >= counter:
+        counter = int(votes[i])
+        winner = i
+print('Winner: ' + str(candidate[winner]))
+print()
+print('---------------------------------')
+print()
+
+#writes the results into a text document
+with open('./Analysis/Analytics.txt','wt') as results:
+
+    results.write('Election Results:\n\n')
+    results.write('---------------------------------\n\n')
+    results.write('Total Votes: ' + str(total) + '\n\n')
+    results.write('---------------------------------\n\n')
+    for i in range(len(candidate)):
+        results.write(str(candidate[i]) + ": " + str(percentage[i]) + '% (' + str(votes[i]) + ')\n\n')
+    results.write('---------------------------------\n\n')
+    results.write('Winner: ' + str(candidate[winner]) + '\n\n')
+    results.write('---------------------------------\n\n')
